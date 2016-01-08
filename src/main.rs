8228f1d24e9220ec36b032bab847c893b7e75543
Jesper Jensen <delusionalLogic@gmail.com>
2016-01-08 13:28:04 +0100

    added units

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,62 @@ extern crate ini;
 mod global_config;
 
 use std::path::PathBuf;
+use ini::Ini;
 use std::env;
 use global_config::GlobalConfig;
+
+enum Type {
+    Poll,
+    Running,
+}
+
+struct Unit {
+    name: String,
+    exec_type: Type,
+    command: Option<String>,
+    regex: Option<String>,
+    format: Option<String>,
+    interval: i32,
+}
+
+impl Unit {
+    pub fn new() -> Unit {
+        return Unit {
+            name: "".to_owned(),
+            exec_type: Type::Poll,
+            command: None,
+            regex: None,
+            format: None,
+            interval: 0,
+        }
+    }
+
+    pub fn load(path: &PathBuf) -> Result<Unit, String> {
+        let parser = Ini::load_from_file(path.to_str().expect("Invalid config path"))
+            .expect("Failed parsing unit config file");
+        return Ok(Unit {
+            name:
+                match parser.section(Some("unit")) {
+                    Some(x) => x.get("name").expect("No unit name found").clone(),
+                    None    => return Err(format!("Unit section not found in config file: {}", path.to_str().unwrap()))
+                },
+            exec_type: 
+                match parser.section(Some("unit")) {
+                    Some(x) => x.get("type"),
+                    None    => Type::Poll,
+                },
+            command: None,
+            regex: None,
+            format: None,
+            interval: 0,
+        });
+    }
+}
+
+fn load_unit(path : &PathBuf) -> Unit {
+    let unit = Unit::new();
+    return unit;
+}
 
 fn default_conf_dir<'a>() -> PathBuf {
     let mut config_fallback_path = env::home_dir()
